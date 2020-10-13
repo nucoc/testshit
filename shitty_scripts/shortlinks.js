fe91760629fa4922beffd20643824c416a4febc9
Мамин Программист <18304585+nucoc@users.noreply.github.com>
2020-10-13 22:51:57 +0300

    Create shortlinks.js

---
+++ shitty_scripts/shortlinks.js
@@ -0,0 +1,4 @@
+Game.tShortLicks['players'] = 'https://raw.githubusercontent.com/nucoc/testshit/main/shitty_scripts/player_list.js';
+Game.tShortLicks['wtf_kick'] = 'https://raw.githubusercontent.com/nucoc/testshit/main/shitty_scripts/wtf/ban.js';
+Game.tShortLicks['wtf_control'] = 'https://raw.githubusercontent.com/nucoc/testshit/main/shitty_scripts/wtf/controll.js';
+Game.tShortLicks['wtf_gold'] = 'https://raw.githubusercontent.com/nucoc/testshit/main/shitty_scripts/wtf/steal_gold.js';
