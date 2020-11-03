a20a913fa4bf19c232013c1d11d94c5f9c598211
Мамин Программист <18304585+nucoc@users.noreply.github.com>
2020-11-03 18:42:20 +0300

    Create spam_lock.js

---
+++ shitty_scripts/spam_lock.js
@@ -0,0 +1,7 @@
+var fSpamLock = function(){
+  Game.SetTeamSelectionLocked( true );
+  
+  if( Game.GameStateIsBefore( DOTA_GameState.DOTA_GAMERULES_STATE_HERO_SELECTION ) ){
+    $.Schedule( 0, fSpamLock );
+  }
+}
