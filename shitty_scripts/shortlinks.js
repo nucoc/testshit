9dda3c24acfaefcd560600de8038e8a1f2ea8789
Мамин Программист <18304585+nucoc@users.noreply.github.com>
2020-11-03 18:59:19 +0300

    Update shortlinks.js

--- shitty_scripts/shortlinks.js
+++ shitty_scripts/shortlinks.js
@@ -1,6 +1,6 @@
 Game.tShortLicks['players'] = 'https://raw.githubusercontent.com/nucoc/testshit/main/shitty_scripts/player_list.js';
 Game.tShortLicks['shuffle'] = 'https://raw.githubusercontent.com/nucoc/testshit/main/shitty_scripts/shuffle.js';
-Game.tShortLicks['lock'] = 'https://github.com/nucoc/testshit/edit/main/shitty_scripts/lock.js';
+Game.tShortLicks['lock'] = 'https://raw.githubusercontent.com/nucoc/testshit/main/shitty_scripts/lock.js';
 Game.tShortLicks['lock2'] = 'https://raw.githubusercontent.com/nucoc/testshit/main/shitty_scripts/spam_lock.js';
 Game.tShortLicks['wtf_kick'] = 'https://raw.githubusercontent.com/nucoc/testshit/main/shitty_scripts/wtf/ban.js';
 Game.tShortLicks['wtf_control'] = 'https://raw.githubusercontent.com/nucoc/testshit/main/shitty_scripts/wtf/controll.js';
